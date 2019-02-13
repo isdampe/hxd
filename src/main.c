15135812ce22dd94866faf8b078b440be46af706
Richard Denton <isdampe@gmail.com>
2019-02-13 15:15:37 +1100

    ENHANCE: Hook up new CLI options for session modes.

--- src/main.c
+++ src/main.c
@@ -11,22 +11,25 @@
 static void hxd_usage()
 {
 	printf("Usage: hxd [options] input_file\nOptions:\n");
-	printf("\t-l  The number of bytes per line\n");
-	printf("\t-g  The number of bytes per group\n");
-	printf("\t-h  Show usage instructions\n");
+	printf("\t-l   The number of bytes per line\n");
+	printf("\t-g   The number of bytes per group\n");
+	printf("\t-h   Show usage instructions\n");
+	printf("\t-t  Only display sections that match the given text\n");
+	printf("\t-b  Only display sections that match the given byte sequence\n");
 	exit(EXIT_FAILURE);
 }
 
 int main(const int argc, char **argv)
 {
-	char *input_fp;
+	char *input_fp, *search_ptr;
 	int opt, bytes_per_line = HXD_DEFAULT_BYTES_PER_LINE,
 		bytes_per_group = HXD_DEFAULT_BYTES_PER_GROUP;
+	enum hxd_session_type session_type = HXD_SESSION_TYPE_STD;
 
 	if (argc < 2)
 		hxd_usage();
 
-	while((opt = getopt(argc, argv, "l:g:h:")) != -1) {
+	while((opt = getopt(argc, argv, "l:g:h:t:b:")) != -1) {
 		switch (opt) {
 			case 'l':
 				bytes_per_line = atoi(optarg);
@@ -37,13 +40,21 @@ int main(const int argc, char **argv)
 			case 'h':
 				hxd_usage();
 				break;
+			case 't':
+				session_type = HXD_SESSION_TYPE_FIND_TEXT;
+				search_ptr = optarg;
+				break;
+			case 'b':
+				session_type = HXD_SESSION_TYPE_FIND_BYTES;
+				search_ptr = optarg;
+				break;
 		}
 	}
 
 	input_fp = argv[argc -1];
 
-	struct hxd_session session = hxd_create_session(input_fp, bytes_per_line,
-		bytes_per_group);
+	struct hxd_session session = hxd_create_session(session_type, input_fp, 
+		bytes_per_line, bytes_per_group);
 	hxd_process(&session);
 	hxd_destroy_session(&session);
 	return 0;

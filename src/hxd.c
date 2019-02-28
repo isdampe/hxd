867b5cacd04514e14cc57a0f3c342bd4b32613e3
Richard Denton <isdampe@gmail.com>
2019-02-28 23:48:23 +0800

    ENHANCE: Implement basic text and byte search. Note: Needs optimisations.

--- src/hxd.c
+++ src/hxd.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "hxd.h"
 
 struct hxd_session hxd_create_session(enum hxd_session_type session_type,
@@ -38,14 +40,15 @@ void hxd_destroy_session(struct hxd_session *session)
 
 void hxd_process_std(struct hxd_session *session)
 {
-	process_output(session, 0x0, -1);
+	process_output(session, 0x0, -1, HXD_NO_HIGHLIGHT, HXD_NO_HIGHLIGHT);
 }
 
-static void process_output(struct hxd_session *session, int offset_start, 
-	int max_length)
+static void process_output(struct hxd_session *session, const int offset_start, 
+	int max_length, const int highlight_start, const int highlight_end)
 {
 	uint8_t input_buffer[session->bytes_per_line];
 	FILE *fh = fopen(session->input_fp, "r");
+	assert(fh != NULL);
 	if (fh == NULL) {
 		fprintf(stderr, "There was an error opening %s.\n", session->input_fp);
 		exit(EXIT_FAILURE);
@@ -70,7 +73,7 @@ static void process_output(struct hxd_session *session, int offset_start,
 
 	while (offset < session->file_size && bytes_read < max_length) {
 		bytes_read = fread(input_buffer, 1, session->bytes_per_line, fh);
-		render_line(bytes_read, input_buffer, offset, session);
+		render_line(bytes_read, input_buffer, offset, session, highlight_start, highlight_end);
 		offset += bytes_read;
 		fseek(fh, offset, SEEK_SET);
 	}
@@ -103,7 +106,8 @@ void hxd_process_text_search(struct hxd_session *session, const char *subject)
 		if (input_buffer[0] == subject[search_idx]) {
 			search_idx++;
 			if (search_idx >= search_end) {
-				process_output(session, offset - (2 * search_end), (2 * search_end));
+				process_output(session, offset - (2 * search_end), (2 * search_end), search_idx,
+					(search_idx + search_end -1));
 				search_idx = 0;
 			}
 		} else {
@@ -120,11 +124,84 @@ void hxd_process_text_search(struct hxd_session *session, const char *subject)
 
 void hxd_process_byte_search(struct hxd_session *session, const char *subject)
 {
-	fprintf(stderr, HXD_COLOR_RED "Byte search isn't implemented yet.\n");
+	if (subject == NULL) {
+		fprintf(stderr, "No string of bytes was provided.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	size_t slength = strlen(subject);
+	if ((slength % 2) != 0) {
+		fprintf(stderr, "You must provide an even number of characters in the byte sequence.\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	size_t dlength = slength / 2;
+
+	uint8_t data[dlength];
+	memset(data, 0, dlength);
+
+	size_t index = 0;
+	while (index < slength) {
+		char c = subject[index];
+		int value = 0;
+		if (c >= '0' && c <= '9')
+			value = (c - '0');
+		else if (c >= 'A' && c <= 'F')
+			value = (10 + (c - 'A'));
+		else if (c >= 'a' && c <= 'f')
+			value = (10 + (c - 'a'));
+		else {
+			fprintf(stderr, "An invalid byte string was provided.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		data[(index / 2)] += value << (((index + 1) % 2) * 4);
+
+		index++;
+	}
+
+	uint8_t input_buffer[2];
+	FILE *fh = fopen(session->input_fp, "r");
+	if (fh == NULL) {
+		fprintf(stderr, "There was an error opening %s.\n", session->input_fp);
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(fh, 0x0, SEEK_END);
+	session->file_size = ftell(fh);
+	rewind(fh);
+
+	int offset = 0x0, bytes_read = 0x0;
+	int search_idx = 0x0, search_end = dlength;
+
+	while (offset < session->file_size) {
+		bytes_read = fread(&input_buffer, 1, 1, fh);
+		offset += bytes_read;
+
+		if (input_buffer[0] == data[search_idx]) {
+			search_idx++;
+			if (search_idx >= search_end) {
+				process_output(session, offset - (2 * search_end), (2 * search_end), search_idx,
+						(search_idx + search_end -1));
+				search_idx = 0;
+			}
+		} else {
+			search_idx = 0;
+		}
+
+		fseek(fh, offset, SEEK_SET);
+	}
+
+	printf(HXD_COLOR_RESET);
+
+	fclose(fh);
+
+
 }
 
 static void render_line(const int bytes_read, uint8_t *input_buffer, const int 
-	offset, struct hxd_session *session)
+		offset, struct hxd_session *session, const int highlight_start, const int highlight_end)
 {
 	char b[9];
 	sprintf(b, "0x%%0%ix  ", (int)(log(session->file_size) / log(16)) +2);
@@ -134,7 +211,7 @@ static void render_line(const int bytes_read, uint8_t *input_buffer, const int
 	printf(b, offset);
 	for (int i=0; i < session->bytes_per_line; ++i) {
 		if (i < bytes_read)
-			render_byte(input_buffer[i]);
+			render_byte(input_buffer[i], (i >= highlight_start && i <= highlight_end ? true : false));
 		else
 			printf("  ");
 
@@ -145,7 +222,7 @@ static void render_line(const int bytes_read, uint8_t *input_buffer, const int
 
 	for (int i=0; i < session->bytes_per_line; ++i) {
 		if (i < bytes_read)
-			render_ascii(input_buffer[i]);
+			render_ascii(input_buffer[i], (i >= highlight_start && i <= highlight_end ? true : false));
 		else
 			printf(" ");
 	}
@@ -154,9 +231,12 @@ static void render_line(const int bytes_read, uint8_t *input_buffer, const int
 
 }
 
-static inline void render_byte(const uint8_t b)
+static inline void render_byte(const uint8_t b, bool highlight)
 {
-	if (b == 0x0) {
+	if (highlight) {
+		printf(HXD_BG_COLOR_GREEN);
+		printf(HXD_COLOR_BLACK "%02x", b);
+	} else if (b == 0x0) {
 		printf(HXD_COLOR_MAGENTA "%02x", b);
 	} else if (b >= 9 && b <= 13) {
 		printf(HXD_COLOR_BLUE "%02x", b);
@@ -165,10 +245,21 @@ static inline void render_byte(const uint8_t b)
 	} else {
 		printf(HXD_COLOR_RED "%02x", b);
 	}
+	printf(HXD_BG_COLOR_RESET);
 }
 
-static inline void render_ascii(const uint8_t b) 
+static inline void render_ascii(const uint8_t b, bool highlight) 
 {
+	if (highlight) { 
+		printf(HXD_BG_COLOR_GREEN);
+		if (b >=33 && b <= 126)
+			printf(HXD_COLOR_BLACK "%c", b);
+		else
+			printf(HXD_COLOR_BLACK ".");
+		return;
+	}
+	
+	printf(HXD_BG_COLOR_RESET);
 	if (b == 0x0) {
 		printf(HXD_COLOR_MAGENTA ".");
 	} else if (b >= 9 && b <= 13) {
--- src/hxd.h
+++ src/hxd.h
@@ -1,10 +1,14 @@
 #ifndef HXD_H
 #include <stdint.h>
+#include <stdbool.h>
 #define HXD_H
 
 #define HXD_DEFAULT_BYTES_PER_LINE 32
 #define HXD_DEFAULT_BYTES_PER_GROUP 8
 
+#define HXD_NO_HIGHLIGHT -1
+
+#define HXD_COLOR_BLACK   "\x1b[30m"
 #define HXD_COLOR_RED     "\x1b[31m"
 #define HXD_COLOR_GREEN   "\x1b[32m"
 #define HXD_COLOR_YELLOW  "\x1b[33m"
@@ -12,6 +16,8 @@
 #define HXD_COLOR_MAGENTA "\x1b[35m"
 #define HXD_COLOR_CYAN    "\x1b[36m"
 #define HXD_COLOR_RESET   "\x1b[0m"
+#define HXD_BG_COLOR_GREEN "\x1b[46m"
+#define HXD_BG_COLOR_RESET "\x1b[0m"
 
 enum hxd_session_type {
 	HXD_SESSION_TYPE_STD,
@@ -31,13 +37,14 @@ struct hxd_session hxd_create_session(enum hxd_session_type session_type,
 	const char *input_fp, const int bytes_per_line, const int bytes_per_group);
 void hxd_destroy_session(struct hxd_session *session);
 void hxd_process_std(struct hxd_session *session);
-static void process_output(struct hxd_session *session, int offset_start, 
-	int max_length);
+static void process_output(struct hxd_session *session, const int offset_start, 
+	int max_length, const int highlight_start, const int highlight_end);
 void hxd_process_text_search(struct hxd_session *session, const char *subject);
 void hxd_process_byte_search(struct hxd_session *session, const char *subject);
 static void render_line(const int bytes_read, uint8_t *input_buffer, const int 
-	offset, struct hxd_session *session);
-static inline void render_byte(const uint8_t b);
-static inline void render_ascii(const uint8_t b);
+	offset, struct hxd_session *session, const int highlight_start, 
+	const int highlight_end);
+static inline void render_byte(const uint8_t b, const bool highlight);
+static inline void render_ascii(const uint8_t b, const bool highlight);
 
 #endif
